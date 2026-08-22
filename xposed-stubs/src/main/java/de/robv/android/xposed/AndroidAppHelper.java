73bcc13b039b93eb6688d0e0ed48b15c81373855
Lycidias93 <42116872+Lycidias93@users.noreply.github.com>
2026-08-22 13:45:37 +0200

    Add AndroidAppHelper compile stub

---
+++ xposed-stubs/src/main/java/de/robv/android/xposed/AndroidAppHelper.java
@@ -0,0 +1,11 @@
+package de.robv.android.xposed;
+
+import android.app.Application;
+
+public final class AndroidAppHelper {
+    private AndroidAppHelper() {}
+
+    public static Application currentApplication() {
+        return null;
+    }
+}
