b761768f2762db6c7f07dc5d27141cf781ad2a73
Lycidias93 <42116872+Lycidias93@users.noreply.github.com>
2026-08-22 13:46:45 +0200

    Keep AndroidAppHelper stub Java-only

--- xposed-stubs/src/main/java/de/robv/android/xposed/AndroidAppHelper.java
+++ xposed-stubs/src/main/java/de/robv/android/xposed/AndroidAppHelper.java
@@ -1,11 +1,9 @@
 package de.robv.android.xposed;
 
-import android.app.Application;
-
 public final class AndroidAppHelper {
     private AndroidAppHelper() {}
 
-    public static Application currentApplication() {
+    public static Object currentApplication() {
         return null;
     }
 }
